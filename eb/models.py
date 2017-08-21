a8ca019193632fa9a61d39d5bd2b278bfc04001a
YangWanjun <ywjsailor@gmail.com>
2017-08-21 15:53:07 +0900

    不具合修正

--- eb/models.py
+++ eb/models.py
@@ -2858,10 +2858,10 @@ class MemberAttendance(BaseModel):
                 # 待機案件の場合、残業と欠勤を計算する必要がない。
                 return 0
             elif total_hours > contract.allowance_time_max:
-                overtime = total_hours - contract.allowance_time_max
+                overtime = total_hours - float(contract.allowance_time_max)
                 return overtime
             else:
-                absenteeism = total_hours - contract.allowance_time_min
+                absenteeism = total_hours - float(contract.allowance_time_min)
                 return absenteeism
         else:
             return 0
