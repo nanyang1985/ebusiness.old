c71273d4ea400394269f1c21b74deab1c8dade18
YangWanjun <ywjsailor@gmail.com>
2019-05-07 15:28:20 +0900

    和暦を西暦に

--- utils/common.py
+++ utils/common.py
@@ -1084,15 +1084,8 @@ def is_cross_date(dates, d, index):
 
 
 def to_wareki(date):
-    if 1926 <= date.year <= 1988:
-        prefix = u"昭和"
-        years = date.year - 1925
-    elif 1989 <= date.year:
-        prefix = u"平成"
-        years = date.year - 1988
-    else:
-        prefix = ''
-        years = date.year
+    prefix = ''
+    years = date.year
     return u"%s%s年%02d月%02d日" % (prefix, years, date.month, date.day)
 
 
